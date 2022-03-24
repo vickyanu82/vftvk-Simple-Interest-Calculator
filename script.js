d06c93cad35658c831ac520175950e1542cea771
vicky <vicky1942in@gmail.com>
2022-03-24 07:15:16 +0530

    Changes done n assignment

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function compute()
 
   var year = new Date().getFullYear()+parseInt(years);
 
-  var amount = principal+interest;
+  
 
-  document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br>in the year "+year+"\<br\>"
+  document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br>in the year "+year+"\<br\>"
 }
 
 function updateRate()
